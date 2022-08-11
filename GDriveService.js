ae5e1b1547f9ce0c20746f39b473dbb9b9011c56
Efimj <87390257+Efimj@users.noreply.github.com>
2022-08-11 11:45:19 +0300

    Update GDriveService.js

--- GDriveService.js
+++ GDriveService.js
@@ -1,80 +1,68 @@
 import { google } from 'googleapis'
 
-const keyFilePath = 'GDkey.json'
-const Scopes = ['https://www.googleapis.com/auth/drive']
-
-const auth = new google.auth.GoogleAuth({
-    keyFile: keyFilePath,
-    scopes: Scopes
-})
-
-const driveService = google.drive({ version: 'v3', auth });
-
-
-
-export const createFile = async (directoryPath, fileName, file) => {
-    
-    const fileMetaData = {
-        parents: [directoryPath],
-        name: fileName
+class Drive {
+    constructor(keyFile, scopes) {
+        const auth = new google.auth.GoogleAuth({
+            keyFile: keyFile,
+            scopes: scopes
+        })
+        this.driveService = google.drive({ version: 'v3', auth });
     }
 
-    const media = {
-        body: file
+    createFile = async (directoryPath, fileName, file) => {
+
+        return await this.driveService.files.create({
+            requestBody: {
+                parents: [directoryPath],
+                name: fileName
+            },
+            media: {
+                body: file
+            },
+        });
     }
 
-    const res = await driveService.files.create({
-        requestBody: fileMetaData,
-        media: media,
-    });
+    updateFile = async (fileId, file) => {
 
-    return res
-}
-
-export const updateFile = async (fileId, file) => {
-
-    const media = {
-        body: file,
+        return await this.driveService.files.update({
+            fileId: fileId,
+            media: {
+                body: file,
+            },
+        });
     }
 
-    const res = await driveService.files.update({
-        fileId: fileId,
-        media: media,
-    });
-    
-    return res
+    deleteFile = async (fileId) => {
+
+        return await this.driveService.files.delete({
+            fileId: fileId,
+        });
+
+    }
+
+    getFileLink = async (fileId) => {
+
+        await this.driveService.permissions.create({
+            fileId: fileId,
+            requestBody: { role: 'reader', type: 'anyone' }
+        })
+
+        return await this.driveService.files.get({
+            fileId: fileId,
+            fields: 'webViewLink, webContentLink, id'
+        });
+    }
+
+    getFile = async (fileId) => {
+
+        return await this.driveService.files.get({
+            fileId: fileId,
+            alt: 'media'
+        });
+    }
 }
 
-export const deleteFile = async (fileId) => {
 
-    const res = await driveService.files.delete({
-        fileId: fileId,
-    });
-
-    return res
-}
-
-export const getFileLink = async (fileId) => {
-
-    await driveService.permissions.create({
-        fileId: fileId,
-        requestBody: { role: 'reader', type: 'anyone' }
-    })
-
-    const res = await driveService.files.get({
-        fileId: fileId,
-        fields: 'webViewLink, webContentLink, id'
-    });
-
-    return res
-}
-
-export const getFile = async (fileId) => {
-
-    const res = await driveService.files.get({
-        fileId: fileId,
-        alt: 'media'
-    });
-
-    return res
+export const createGDrive = (keyPath = 'GDkey.json', scopes = 'https://www.googleapis.com/auth/drive') => {
+    return new Drive(keyPath, scopes)
 }
